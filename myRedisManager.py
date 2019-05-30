667a223f17fc0978784809000048a7676f40c54c
imBobby-MeiYa <bjw9808@163.com>
2019-05-30 11:38:15 +0800

    1.添加TreeView显示控件 2.添加连接数据库的异常处理

--- myRedisManager.py
+++ myRedisManager.py
@@ -1,29 +1,59 @@
+# 2019.5.30
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 from tkinter import *
+import redis
+import re
+import logging
+logging.basicConfig(level=logging.INFO)
 
 def do_job():
     messagebox.showinfo(message = "myRedisManager Version 0.0.1", title = "myRedisManager")
 
+def detect_ip_input(redis_address):
+    """
+    用正则表达式检查IP地址输入是否正确
+    :param redis_address:
+    :return:1：输入正确 0：输入错误
+    """
+    if re.findall(r'\b(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b',redis_address):
+        return 1
+    else:
+        return 0
+
 def window_config(window_width, window_height, root_window):
     def redis_connect():
-        # 异常处理
+        # 获取entry控件内的数据
         redis_address = Entry_address.get()
         redis_port = Entry_port.get()
         redis_password = Entry_password.get()
-        if redis_address and redis_port and redis_password:
-            messagebox.showinfo(message=(redis_address + redis_port + redis_password), title='输入正常')
+        redis_DB_num = Entry_DB_num.get()
+        # 尝试连接
+        if redis_address and redis_port and redis_password and detect_ip_input(redis_address):
+            # 清除输入框内容
             Entry_address.delete(0, END)
             Entry_port.delete(0, END)
             Entry_password.delete(0, END)
+            Entry_DB_num.delete(0, END)
+            try:
+                r = redis.StrictRedis(host=redis_address, port=redis_port, password=redis_password, db=redis_DB_num,
+                                      socket_connect_timeout=3)
+                logging.info(str(r.get('NetStatus')))
+            except ConnectionRefusedError as e:
+                messagebox.showinfo(message='错误IP地址提示:\n' + str(e), title='提示')
+            except redis.exceptions.ConnectionError as e:
+                messagebox.showinfo(message='错误端口号提示：\n' + str(e), title='提示')
+            except redis.exceptions.ResponseError as e:
+                messagebox.showinfo(message='错误连接密码提示：\n' + str(e), title='提示')
+            except redis.exceptions.TimeoutError as e:
+                messagebox.showinfo(message='连接超时提示：\n' + str(e), title='提示')
         else:
-            messagebox.showinfo(message='输入不能为空', title='异常提示')
+            messagebox.showinfo(message='输入错误，请检查', title='异常提示')
             Entry_address.delete(0, END)
             Entry_port.delete(0, END)
             Entry_password.delete(0, END)
-        # message = 'redis_address:' + redis_address + '\n' + 'port' + redis_port + 'password' + redis_password
-
-        # messagebox.showinfo(message=message, title="myRedisManager")
+            Entry_DB_num.delete(0, END)
 
     # 配置窗口为居中
     width_screen = root_window.winfo_screenwidth()
@@ -57,17 +87,29 @@ def window_config(window_width, window_height, root_window):
     Label_address.grid(row=0,column=0)
     Entry_address = Entry(root_window)
     Entry_address.grid(row=0,column=1)
+
     Label_port = Label(root_window, text='Redis服务器端口号：')
     Label_port.grid(row=1, column=0)
     Entry_port = Entry(root_window)
     Entry_port.grid(row=1, column=1)
+
+    Label_DB_num = Label(root_window, text='RedisDB号：')
+    Label_DB_num.grid(row=2, column=0)
+    Entry_DB_num = Entry(root_window)
+    Entry_DB_num.grid(row=2, column=1)
+
     Label_password = Label(root_window, text='Redis服务器密码：')
-    Label_password.grid(row=2, column=0)
+    Label_password.grid(row=3, column=0)
     Entry_password = Entry(root_window, show='*')
-    Entry_password.grid(row=2, column=1)
+    Entry_password.grid(row=3, column=1)
+
     button_connect = Button(text='连接', command=redis_connect)
-    button_connect.grid(row=3, column=0)
+    button_connect.grid(row=4, column=0)
+
+    tree = ttk.Treeview(root_window, columns=['1', '2', '3', '4', '5'], show='headings')
+    tree.grid(row=5, column=0, columnspan=6)
+    tree.insert('', 'end', values='11')
 
 root_window = tk.Tk()
-window_config(500, 300, root_window)
+window_config(900, 350, root_window)
 root_window.mainloop()
